d96cf705c643f18d120de67f82ab308b6de8baf5
ehsankhan-blitz <ehsan.khan@blitzscale.co>
2022-09-16 12:20:36 +0530

    added routes setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Calendar from './pages/Calendar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/calendar' element={<Calendar/>} />
+      </Routes>
+   
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+// import React from "react";
+// import Login from "/pages/Login";
+// import Home from "/pages/Home";
+// import Calendar from "./pages/Calendar";
+
+// export default [
+//   {
+//     path: '/login',
+//     isActive: true,                                                                    
+//     showOnSider: true,
+//     component: Login,
+//     // allowedRoles: PAGE_ACCESS_ROLES['reports-summary'] 
+//   },
+//   {
+//     path: '/home',
+//     isActive: true,                                                                   
+//     showOnSider: true,
+//     component: Home,
+//     // allowedRoles: PAGE_ACCESS_ROLES['reports-summary'] 
+//   },
+//   {
+//     path: '/calender',
+//     isActive: true,                                                                    
+//     showOnSider: true,
+//     component: Calendar,
+//     // allowedRoles: PAGE_ACCESS_ROLES['reports-summary'] 
+//   }
+// ]
